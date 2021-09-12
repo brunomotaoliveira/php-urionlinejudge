0e3d65f673dbec9040a3f715c5ac26d83d2a0c1e
brunomotadev <brunomotadev@gmail.com>
2021-09-11 21:03:40 -0300

    adicionado o barra n no final do echo

--- iniciante/media2_1006.php
+++ iniciante/media2_1006.php
@@ -6,5 +6,5 @@ $valorC = fgets(STDIN);
 $media = (($valorA * 2) + ($valorB * 3) + ($valorC * 5)) / 10;
 
 $media = number_format($media,1,".","");
-echo "MEDIA = {$media}";
+echo "MEDIA = {$media}\n";
 ?>
