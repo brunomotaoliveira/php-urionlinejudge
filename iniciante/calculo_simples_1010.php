c96b5620d9cc2f3075d01fb5c4f8b6d9041a9e3e
brunomotadev <brunomotadev@gmail.com>
2021-09-13 02:40:37 -0300

    explicação do exercicio adicionado nos comentarios

--- iniciante/calculo_simples_1010.php
+++ iniciante/calculo_simples_1010.php
@@ -13,6 +13,16 @@
 
     $valorPago = number_format($valorPago,2,'.','');
     echo "VALOR A PAGAR: R$ {$valorPago}\n";
-
+/**
+ * explode => Divide uma string em strings, utilizando no caso o espaço " ". Como parâmetro, se tornando uma array. Assim a variavel $linha1, se torna uma array.
+ * 
+ * trim => remove espaços do que for capturado pelo fgets.
+ * 
+ *  fgets => captura os dados, no caso o que for digitado pelo teclado, parametro STDIN.
+ * 
+ *  intval => vai converter o valor em inteiro. No caso o $linha[0]. A string armazenada na array na posição 0, será convertida em inteiro e depois armazenado em $codigoDaPeca1.
+ * 
+ *  doubleval ou floatval => assim como o intval que converte em inteiro. O floatval irá converter a string em ponto flutuante(decimal). OBS: doubleval é um "apelido" para floatval, ambos funcionam da mesma forma.
+ */
     
 ?>
