714527dde608704541438d1a329b4a69cbb756e0
brunomotadev <brunomotadev@gmail.com>
2021-09-14 05:55:49 -0300

    esfera 1011

---
+++ iniciante/esfera_1011.php
@@ -0,0 +1,10 @@
+<?php 
+    $raioDaEsfera = trim(fgets(STDIN));
+    $pi = 3.14159;
+
+    $calcularVolume = (4/3) * $pi;
+    $calcularVolume = $calcularVolume * ($raioDaEsfera ** 3);
+
+    $calcularVolume = number_format($calcularVolume,3,'.','');
+    echo "VOLUME = {$calcularVolume}";
+?>
