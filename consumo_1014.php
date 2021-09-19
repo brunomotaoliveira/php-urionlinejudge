461a2531bcc1c5a1c58bf0abda2578af9b06e3d1
brunomotadev <brunomotadev@gmail.com>
2021-09-18 22:37:31 -0300

    movido exercicio 1014

--- consumo_1014.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$distanciaTotalPercorrida = trim(fgets(STDIN));
-$totalDeCombustivelGasto = trim(fgets(STDIN));
-
-$consumoMedio = $distanciaTotalPercorrida / $totalDeCombustivelGasto;
-$consumoMedio = number_format($consumoMedio,3,'.','');
-echo "{$consumoMedio} km/l\n";
-?>
