939cb92ad1a34a734a7cab4269095bab10343b1c
brunomotadev <brunomotadev@gmail.com>
2021-09-18 22:07:08 -0300

    o maior - 1013

---
+++ iniciante/omaior_1013.php
@@ -0,0 +1,11 @@
+<?php 
+$lendoTresValores = explode(" ",trim(fgets(STDIN)));
+$valorA = intval($lendoTresValores[0]);
+$valorB = intval($lendoTresValores[1]);
+$valorC = intval($lendoTresValores[2]);
+
+$maiorAB = ($valorA + $valorB + abs($valorB - $valorA)) /2; // abs = retorna o valor absoluto
+$maiorAB = ($valorC + $maiorAB + abs($maiorAB - $valorC)) /2;
+
+echo "{$maiorAB} eh o maior";
+?>
